d9bd56f86ab633e74651e6b994c664e5fa540633
Christian <chemker@rhdc.de>
2025-08-21 12:12:46 +0200

    Initial Commit

---
+++ LanePace/LanePaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanePaceApp.swift
+//  LanePace
+//
+//  Created by Christian Hemker on 21.08.25.
+//
+
+import SwiftUI
+
+@main
+struct LanePaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LanePaceTests/LanePaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  LanePaceTests.swift
+//  LanePaceTests
+//
+//  Created by Christian Hemker on 21.08.25.
+//
+
+import Testing
+@testable import LanePace
+
+struct LanePaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
